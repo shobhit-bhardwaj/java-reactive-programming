14c364bb01259e9f3abb693365b77a5860e3fe0d
Shobhit Bhardwaj <successshobhit@gmail.com>
2023-06-17 21:24:23 +0530

    Exercise 3

--- java-reactive-samples/src/main/java/com/shobhit/reactive/Exercise3.java
+++ java-reactive-samples/src/main/java/com/shobhit/reactive/Exercise3.java
@@ -2,8 +2,11 @@ package com.shobhit.reactive;
 
 import java.util.List;
 
+import com.shobhit.reactive.models.User;
+import com.shobhit.reactive.sources.ReactiveSources;
+
 public class Exercise3 {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 
 		//	Get all Users from a ReactiveSource userFlux and dump into a list
 		//	and print it.
